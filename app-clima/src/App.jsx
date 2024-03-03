ec42e3a2b0e3d5eee6820392f033b8214944dd6b
NelsonGrajales <ngrajales2332@gmail.com>
2024-03-02 21:47:54 -0500

    estilos

--- app-clima/src/App.jsx
+++ app-clima/src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import sun from '../src/assets/01_sunny_color_w32.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='contenedor'>
-      <div className='info'>
-        <p className='ciudad'>Venice, CA</p>
-        <span className='temperatura'>23/20</span>
-        <div className='datos'>
-          <span>Wind <br /> sw 25km/h</span>
-          <span>Humidity <br /> 75%</span>
-          <span>uv index  <br /> 0/10</span>
+    <div className='contenedor'> 
+      <div className='city'>
+        <div className='top'>
+          <span>New York</span>
+          <span>09:30</span>
         </div>
-      </div>
-      <div className="luna"></div>   
-       
+        <img className="img" src={sun} alt="" />                 
+        <div className='bottom'>
+          <span>AQUI 70</span>          
+          <span> -5-0</span>   
+        </div>
+      </div> 
     </div>      
   )
 }
