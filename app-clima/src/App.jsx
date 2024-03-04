2f44cb73b06be96c68fdbc9ce3318a27afd13cb1
NelsonGrajales <ngrajales2332@gmail.com>
2024-03-03 23:22:59 -0500

    cambios

--- app-clima/src/App.jsx
+++ app-clima/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import sun from '../src/assets/01_sunny_color_w32.png'
+import { weather } from './services/clima'
 import './App.css'
 
 function App() {
@@ -9,17 +10,14 @@ function App() {
 
   return (
     <div className='contenedor'> 
-      <div className='city'>
-        <div className='top'>
-          <span>New York</span>
-          <span>09:30</span>
-        </div>
-        <img className="img" src={sun} alt="" />                 
-        <div className='bottom'>
-          <span>AQUI 70</span>          
-          <span> -5-0</span>   
-        </div>
-      </div> 
+      <div className='d'>
+        <h1 className='b'>Hongkong</h1>
+        <p className='c'>09:30 • 03/08</p>
+        <span className='g'>24°</span>
+        <img className='l' src={sun} alt="" />
+        <div></div>
+      </div>
+      
     </div>      
   )
 }
--- app-clima/src/services/clima.js
+++ app-clima/src/services/clima.js
@@ -4,12 +4,15 @@ const API_KEY = 'db9c32c5674a4bd8a6c41226240103';
 export const weather = async ({city}) => {
     if (city === '') {
         city = 'Panama';
-
-        try{
-            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
-            const json = await response.json()
-        }catch(e){
-            throw new Error('Error searching city')
-        }
+    }
+    try{
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
+        const json = await response.json()
+        return json
+    }catch(e){
+        throw new Error('Error searching city')
     }
 }
+
+
+weather()
