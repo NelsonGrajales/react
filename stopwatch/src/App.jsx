29db45f92caf1ac3cf7b354fb7bd05a3a1def407
NelsonGrajales <ngrajales2332@gmail.com>
2024-04-02 23:16:42 -0500

    stopwatch

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import StopWatch from './components/StopWatch'
+
+function App() {
+
+  return (
+    <div>
+      <StopWatch></StopWatch>
+    </div>
+  )
+}
+
+export default App
