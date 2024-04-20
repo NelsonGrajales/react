3aae5401f1a8540d6d6863f005d1c50953351ac1
NelsonGrajales <ngrajales2332@gmail.com>
2024-04-19 22:54:45 -0500

    ip

--- ipfinder/src/Maps.jsx
+++ ipfinder/src/Maps.jsx
@@ -1,8 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import ReactMapGL, { Maker } from 'react-map-gl';
+import { RiUserLocationFill } from 'react-icons/ri';
+
 function Map({ lat, lon }) {
     return (
         <div>
-            <ReactMapGL>
-
+            <ReactMapGL mapboxApiAccessToken={API_KEY}
+                {...viewport}
+                onViewportChange={(viewport) => setViewport(viewport)}
+                mapStyle="mapbox: //styles/mapboxstreets-v11" >
+                <Marker latitude={lat} longitude={lon}>
+                    <div>
+                        <RiUserLocationFill size="25px" color='blue'></RiUserLocationFill>
+                    </div>
+                </Marker>
             </ReactMapGL>
         </div>
     )
