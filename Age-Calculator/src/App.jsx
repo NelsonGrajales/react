8bb08acabccd2aa0a0e2ff6222766a5b3528ca28
NelsonGrajales <ngrajales2332@gmail.com>
2024-06-28 23:06:56 -0500

    tailwind

--- Age-Calculator/src/App.jsx
+++ Age-Calculator/src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-
+      <p className="text-3xl font-bold underline outline-orange-50">
+        Hello world!
+      </p>
+      <p className=" text-orange-200 text-3xl font-bold underline outline-orange-50">
+        Hello world!
+      </p>
     </>
   )
 }
