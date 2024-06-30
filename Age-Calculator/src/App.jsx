1acc79dbc8114f340efc181f8810f34274ab943d
NelsonGrajales <ngrajales2332@gmail.com>
2024-06-29 23:36:37 -0500

    a

--- Age-Calculator/src/App.jsx
+++ Age-Calculator/src/App.jsx
@@ -4,12 +4,7 @@ function App() {
 
   return (
     <>
-      <p className="text-3xl font-bold underline outline-orange-50">
-        Hello world!
-      </p>
-      <p className=" text-orange-200 text-3xl font-bold underline outline-orange-50">
-        Hello world!
-      </p>
+
     </>
   )
 }
