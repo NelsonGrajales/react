62e17ade256f15390dc8a01cca0d8dd90d93bdf7
NelsonGrajales <ngrajales2332@gmail.com>
2024-06-30 23:22:40 -0500

    estructura

--- Age-Calculator/src/App.jsx
+++ Age-Calculator/src/App.jsx
@@ -1,10 +1,48 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isDay, setIsDay] = useState(0);
+  const [isYear, setIsYear] = useState(0);
+  const [isMonth, setIsMonth] = useState(0);
 
   return (
     <>
+      <form action="/submit" method="post">
+        <div>
+          <label for="nombre">Day</label>
+          <input type="number" id="nombre" name="nombre" required />
+        </div>
+        <div>
+          <label for="email">Month</label>
+          <input type="month" id="email" name="month" required />
+        </div>
+        <div>
+          <label for="pregunta">Year</label>
+          <input type="number" id="pregunta" name="pregunta" required />
+        </div>
+        <button className='submit' type="submit"><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-narrow-down" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 5l0 14" />
+          <path d="M16 15l-4 4" />
+          <path d="M8 15l4 4" />
+        </svg></button>
+      </form>
 
+      <div>
+        <div>
+          <span>{isDay}</span>
+          <h1>years</h1>
+        </div>
+        <div>
+          <span>{isMonth}</span>
+          <h1>month</h1>
+        </div>
+        <div>
+          <span>{isYear}</span>
+          <h1>days</h1>
+        </div>
+      </div>
     </>
   )
 }
